21338e9bbaea889b5b51cae3c9b3c7378e6a64ee
lzq <LZQ_ys@126.com>
2020-04-03 14:29:35 +0800

    新增文件getopts（多看）.sh

---
+++ getopts（多看）.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo
+while getopts :ab:c opt
+do
+	case "$opt" in
+		a) echo "Found the -a option"
+			;;
+		b) echo "Found the -b option, with value $OPTARG"
+			;;
+		c) echo "Found the -c option"
+			;;
+		*)echo "Unknow option : $opt"
+			;;
+	esac
+done
